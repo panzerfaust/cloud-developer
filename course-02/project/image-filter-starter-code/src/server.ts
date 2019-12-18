4e66203a28a2772f8dc6229e0b316886359a22dc
andrewklazinga <andrewklazinga@gmail.com>
2019-12-18 14:33:09 +0200

    completed project server.ts

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import fs from 'fs';
 
 (async () => {
 
@@ -17,7 +18,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
+  //    
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -29,8 +30,51 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
-  //! END @TODO1
-  
+  //! END @TODO1  
+
+  // GET method to valid a URL, filter it using filterImageFromURL, and send it back.
+  // The server is then purged of all stored images.
+  // Example input from Postman:
+  // http://localhost:8082/filteredimage?image_url=https://images.pexels.com/photos/2318555/pexels-photo-2318555.jpeg
+
+  app.get( "/filteredimage/", async ( req: Request, res: Response ) => {
+
+    // checks to see if there is a valid URL
+    let image_url:string = req.query.image_url;
+
+    // checks to see if there is a URL and if not returns requirement
+    if ( !image_url ) {
+      return res.status(422)
+                .send(`valid URL is required`);
+    }
+
+    // inputs URL into filterImageFromURL function
+    // downloads the image to /tmp/, renames it, and applies filtering effects
+    const filteredPath = await filterImageFromURL(image_url);
+
+    // returns the new filtered image
+    res.sendFile(filteredPath);
+
+    // reads local directory and creates an array of the file names
+    fs.readdir(__dirname + "/util/tmp/", async function(err, file) {
+    // console.log(file);
+
+      // adds the absolute address to every element of a new array
+      const filePath : Array<string> = new Array();
+      file.forEach(function(element) {
+        filePath.push(__dirname + "/util/tmp/" + element);
+      });
+
+      // deletes locally stored image and sends error message if there is an issue
+      try{
+      await deleteLocalFiles(filePath);}
+      catch(err){
+        console.log('failure to delete!');
+      }
+    });
+
+   });
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
@@ -43,4 +87,5 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
       console.log( `server running http://localhost:${ port }` );
       console.log( `press CTRL+C to stop server` );
   } );
+
 })();
