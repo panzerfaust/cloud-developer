692c5f75f7350103d935355e266f5a08096fea42
andrewklazinga <andrewklazinga@gmail.com>
2020-01-20 14:19:08 +0200

    push of entire folder

--- course-04/exercises/lesson-4/client/src/config.ts
+++ course-04/exercises/lesson-4/client/src/config.ts
@@ -1,1 +1,1 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://ensas0osgc.execute-api.eu-west-1.amazonaws.com/dev/'
---
+++ course-04/project-4/client/src/config.ts
@@ -0,0 +1,10 @@
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+const apiId = '36azyj26yh'
+export const apiEndpoint = `https://${apiId}.execute-api.eu-west-1.amazonaws.com/dev`
+
+export const authConfig = {
+  // TODO: Create an Auth0 application and copy values from it into this map
+  domain      : 'dev-7z7k9pni.auth0.com',            // Auth0 domain
+  clientId    : '0uOrGQ2g1l5md4C7epRZkVhtWcUyHb5j',  // Auth0 client id
+  callbackUrl : 'http://localhost:3000/callback'
+}
